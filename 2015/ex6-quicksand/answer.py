ab27f7b56e01c0dcd9f42dccf154ccadef598581
glegoux <gilles.legoux@parislabs.net>
2015-05-28 09:57:15 +0200

    add new responses

--- 2015/ex6-quicksand/answer.py
+++ 2015/ex6-quicksand/answer.py
@@ -13,4 +13,35 @@ lines = list()
 for line in sys.stdin:
     lines.append(line.rstrip('\n'))
 
-print("TODO")
+H = int(lines[0].split(' ')[0])
+L = int(lines[0].split(' ')[1])
+del lines[0]
+
+M = list()
+for i in range(H):
+    M.append([0]*L)
+
+E = list()
+
+def distance(case):
+    global i, j
+    return abs(i - case[0]) + abs(j - case[1])
+
+for i, line in enumerate(lines):
+    for j, case in enumerate(line):
+        if case == '.':
+            M[i][j] = 0
+            E.append([i, j])
+
+for i, line in enumerate(lines):
+    for j, case in enumerate(line):
+        if case == '#':
+            M[i][j] = min(list(map(distance, E)))
+
+res = 0
+for i in range(H):
+    for j in range(L):
+        if M[i][j] > res:
+            res = M[i][j]
+
+print(res)
--- 2015/ex7-life-game/answer.py
+++ 2015/ex7-life-game/answer.py
@@ -13,4 +13,61 @@ lines = list()
 for line in sys.stdin:
     lines.append(line.rstrip('\n'))
 
-print("TODO")
+N = lines[0]
+del lines[0]
+
+xmax = 0
+ymax = 0
+for line in lines:
+    l = line.split(' ')
+    x2 = int(l[2])
+    y2 = int(l[3])
+    xmax = max(xmax, x2)
+    ymax = max(ymax, y2)
+
+M = list()
+for y in range(ymax):
+    M.append([0]*xmax)
+
+for line in lines:
+    l = line.split(' ')
+    x1 = int(l[0])
+    y1 = int(l[1])
+    x2 = int(l[2])
+    y2 = int(l[3])
+    for y in range(y1-1,y2):
+        M[y][x1-1:x2] = [1]*(x2-x1+1)
+
+def copy(m):
+    res = list()
+    for y in range(len(m)):
+        res.append(list(m[y]))
+    return res
+
+def death(m):
+    for y in range(len(m)):
+        for e in m[y]:
+            if e == 1:
+                return False
+    return True
+
+res = 0
+MM = copy(M)
+while not(death(M)):
+    for y in range(ymax):
+        for x in range(xmax):
+            if x == 0:
+                MM[y][x] = 0
+                continue
+            if y == 0:
+                MM[y][x] = 0
+                continue
+            if M[y][x-1] == 0 and M[y-1][x] == 0:
+                MM[y][x] = 0
+            elif M[y][x-1] == 1 and M[y-1][x] == 1:
+                MM[y][x] = 1
+    M = MM
+    MM = copy(M)
+    res += 1
+
+print(res)
