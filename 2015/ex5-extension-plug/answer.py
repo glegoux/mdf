337069673dec141612c4454fe3c6a8e825e47157
glegoux <gilles.legoux@parislabs.net>
2015-05-29 00:50:11 +0200

    add ex5

--- 2015/ex5-extension-plug/answer.py
+++ 2015/ex5-extension-plug/answer.py
@@ -13,4 +13,30 @@ lines = list()
 for line in sys.stdin:
     lines.append(line.rstrip('\n'))
 
-print("TODO")
+N = int(lines[0])
+del lines[0]
+
+plugs = dict()
+plugs['F-M'] = list()
+plugs['M-F'] = list()
+plugs['M-M'] = list()
+plugs['F-F'] = list()
+for line in lines:
+    l = line.split(' ')
+    _type = l[0]
+    _len = int(l[1])
+    plugs[_type].append(_len)
+
+for plug in plugs.values():
+    plug.sort()
+    plug.reverse()
+
+longest = sum(plugs['F-M']) + sum(plugs['M-F'])
+mm = len(plugs['M-M'])
+ff = len(plugs['F-F'])
+if mm < ff:
+    longest += sum(plugs['M-M']) + sum(plugs['F-F'][:mm])
+else:
+    longest += sum(plugs['F-F']) + sum(plugs['M-M'][:ff])
+
+print(longest)
