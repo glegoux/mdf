c01839a628d0d0a9e9e7dbad313de11b765e1636
Gilles Legoux <gilles.legoux@gmail.com>
2015-05-29 01:29:29 +0200

    Update answer.py

--- 2015/ex7-life-game/answer.py
+++ 2015/ex7-life-game/answer.py
@@ -26,7 +26,7 @@ for line in lines:
     ymax = max(ymax, y2)
 
 M = list()
-for y in range(ymax):
+for _ in range(ymax):
     M.append([0]*xmax)
 
 for line in lines:
@@ -35,8 +35,8 @@ for line in lines:
     y1 = int(l[1])
     x2 = int(l[2])
     y2 = int(l[3])
-    for y in range(y1-1,y2):
-        M[y][x1-1:x2] = [1]*(x2-x1+1)
+    for y in range(y1 - 1, y2):
+        M[y][x1 - 1:x2] = [1]*(x2 - x1 + 1)
 
 def copy(m):
     res = list()
@@ -44,30 +44,27 @@ def copy(m):
         res.append(list(m[y]))
     return res
 
-def death(m):
+def is_alive(m):
     for y in range(len(m)):
         for e in m[y]:
             if e == 1:
-                return False
-    return True
+                return True
+    return False
 
-res = 0
+lifespan = 0
 MM = copy(M)
-while not(death(M)):
+while is_alive(M):
     for y in range(ymax):
         for x in range(xmax):
-            if x == 0:
+            if x * y == 0:
                 MM[y][x] = 0
                 continue
-            if y == 0:
+            if M[y][x-1] + M[y-1][x] == 0:
                 MM[y][x] = 0
-                continue
-            if M[y][x-1] == 0 and M[y-1][x] == 0:
-                MM[y][x] = 0
-            elif M[y][x-1] == 1 and M[y-1][x] == 1:
+            elif M[y][x-1] * M[y-1][x] == 1:
                 MM[y][x] = 1
     M = MM
     MM = copy(M)
-    res += 1
+    lifespan += 1
 
-print(res)
+print(lifespan)
