a62dc76f3fa46e9da5d153d9d49e4b9e75ff6e8e
Gilles Legoux <gilles.legoux@gmail.com>
2016-03-17 21:14:13 +0100

    Update answer.py

--- 2016/final/ex2/answer.py
+++ 2016/final/ex2/answer.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+
+##
+#  ex2 :
+#  Read input from STDIN.
+#  Use 'print' to output your result, use the '\n' constant at the end of each result line.
+#  Use 'local_print(variable, ... )' to display simple variables in a dedicated area.
+#
+
 import sys
 sys.setrecursionlimit(1000000000)
 n=int(input())
